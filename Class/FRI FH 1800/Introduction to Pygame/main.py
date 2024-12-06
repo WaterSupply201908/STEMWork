48e405dacccb7a2146895383ecdb719a30fb2e41
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2024-12-07 00:30:20 +0800

    2024-12-06 Introduction to Pygame - part 1, 2 & 3 [page 35]

---
+++ Class/FRI FH 1800/Introduction to Pygame/main.py
@@ -0,0 +1,31 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('Sky.png')
+ground_surface = pygame.image.load('ground.png')
+text_surface = test_font.render('My Game', False, 'Black')
+snail_surface = pygame.image.load('snail1.png')
+snail_x_pos = 600
+
+while True :
+    for e in pygame.event.get() :
+        if e.type == pygame.QUIT :
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < 100 :
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 250))
+
+    pygame.display.update()
+    clock.tick(60)
