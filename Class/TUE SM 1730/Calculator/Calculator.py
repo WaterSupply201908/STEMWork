bf9b7296f90f08649464338836aa278709d849d7
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2024-12-11 22:53:30 +0800

    2024-12-10 GUI - (Part 4) [page 12]

--- Class/TUE SM 1730/Calculator/Calculator.py
+++ Class/TUE SM 1730/Calculator/Calculator.py
@@ -3,12 +3,18 @@ import darkdetect
 
 class Calculator(ctk.CTk) :
     def __init__(self, is_dark) :
-        super().__init__()
+        super().__init__(fg_color=('White', 'Black'))
 
         if is_dark :
             ctk.set_appearance_mode('dark')
         else :
             ctk.set_appearance_mode('light')
+
+        self.geometry('400x700')
+        self.resizable(False, False)
+        self.title('Calculator')
+
+        self.rowconfigure()
 
         self.mainloop()
 
