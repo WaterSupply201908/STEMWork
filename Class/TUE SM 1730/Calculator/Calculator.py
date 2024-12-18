9669d9f73a0af296acdfcdcb8e9542611bceea62
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2024-12-18 11:08:57 +0800

    2024-12-17 GUI - (Part 4) [page 15]

--- Class/TUE SM 1730/Calculator/Calculator.py
+++ Class/TUE SM 1730/Calculator/Calculator.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 import darkdetect
+from Setting import *
 
 class Calculator(ctk.CTk) :
     def __init__(self, is_dark) :
@@ -14,9 +15,21 @@ class Calculator(ctk.CTk) :
         self.resizable(False, False)
         self.title('Calculator')
 
-        self.rowconfigure()
+        self.rowconfigure(list(range(MAIN_ROWS)), weight=1, uniform='a')
+        self.columnconfigure(list(range(MAIN_COLUMNS)), weight=1, uniform='a')
 
+        self.create_widgets()
+        
         self.mainloop()
+
+    def create_widgets(self) :
+        OutputLabel(self, 0)
+        OutputLabel(self, 1)
+
+class OutputLabel(ctk.CTkLabel) :
+    def __init__(self, parent, row) :
+        super().__init__(master=parent, text='123')
+        self.grid(column=0, columnspan=4, row=row)
 
 # Main program
 Calculator(darkdetect.isDark())
