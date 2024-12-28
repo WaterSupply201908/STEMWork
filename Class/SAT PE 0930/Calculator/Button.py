1cef7222545108f445fe20a393a8b5e979d50542
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2024-12-28 14:57:24 +0800

    2024-12-28 GUI - (Part 5) [page 11]

--- Class/SAT PE 0930/Calculator/Button.py
+++ Class/SAT PE 0930/Calculator/Button.py
@@ -2,5 +2,41 @@ from customtkinter import CTkButton
 from Setting import *
 
 class Button(CTkButton) :
-    def __init__(self) :
-        pass
+    def __init__(self, parent, text, func, col, row, font, color='dark-gray') :
+        super().__init__(
+            master=parent,
+            command=func,
+            text=text,
+            corner_radius=STYLING['corner-radius'],
+            font=font,
+            fg_color=COLORS[color]['fg'],
+            hover_color=COLORS[color]['hover'],
+            text_color=COLORS[color]['text']
+            )
+        self.grid(column=col, row=row, sticky='NSEW', padx=STYLING['gap'], pady=STYLING['gap'])
+
+class ImageButton(CTkButton) :
+    def __init__(self, parent, func, col, row, image, text='', color='dark-gray') :
+        super().__init__(
+            master=parent,
+            command=func,
+            text=text,
+            corner_radius=STYLING['corner-radius'],
+            image=image,
+            fg_color=COLORS[color]['fg'],
+            hover_color=COLORS[color]['hover'],
+            text_color=COLORS[color]['text']
+            )
+        self.grid(column=col, row=row, sticky='NSEW', padx=STYLING['gap'], pady=STYLING['gap'])
+
+class NumButton(Button) :
+    def __init__(self, parent, text, func, col, row, font, color='light-gray') :
+        super().__init__(
+            parent=parent,
+            func=func,
+            text=text,
+            col=col,
+            row=row,
+            font=font,
+            color=color
+            )
