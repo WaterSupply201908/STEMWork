ad0ad2e5fe4e0c03b84893c4ac45a34fbbd179f9
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2024-12-29 23:21:31 +0800

    2024-12-28 GUI - (Part 1) [pg 21]

---
+++ Class/SAT TW 1530/BMI/bmi.py
@@ -0,0 +1,11 @@
+2024-12-28
+
+Tutor : LAP021
+
+replit.com
+
+TW SAT 15:30/bmi.py
+
+https://drive.google.com/drive/folders/1qMt0LIQJGqq4-YeVG7vfNzHnJcpOfjEk?usp=drive_link
+
+Tutor : codon-bevy0k@icloud.com
