ce9b6c10d706de43e3c41800c67fb4bf9ba88cad
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-01-06 12:20:07 +0800

    2024-12-31 GUI - (Part 4) [Ricci]

--- Class/TUE SM 1730/Calculator/Calculator.py
+++ Class/TUE SM 1730/Calculator/Calculator.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import darkdetect
 from Setting import *
+from Button import Button
 
 class Calculator(ctk.CTk) :
     def __init__(self, is_dark) :
@@ -18,18 +19,45 @@ class Calculator(ctk.CTk) :
         self.rowconfigure(list(range(MAIN_ROWS)), weight=1, uniform='a')
         self.columnconfigure(list(range(MAIN_COLUMNS)), weight=1, uniform='a')
 
+        self.result_string = ctk.StringVar(value= '0')
+        self.formula_string = ctk.StringVar(value='')
+
         self.create_widgets()
         
         self.mainloop()
 
     def create_widgets(self) :
-        OutputLabel(self, 0)
-        OutputLabel(self, 1)
+        main_font = ctk.CTkFont(family=FONT, size=NORMAL_FONT_SIZE)
+        result_font = ctk.CTkFont(family=FONT, size=OUTPUT_FONT_SIZE)
 
-class OutputLabel(ctk.CTkLabel) :
-    def __init__(self, parent, row) :
-        super().__init__(master=parent, text='123')
-        self.grid(column=0, columnspan=4, row=row)
+        OutputLabel(self, 0, 'SE', main_font, self.formula_string)
+        OutputLabel(self, 1, 'E', result_font, self.result_string)
+
+        Button(parent=self, 
+               text=OPERATORS['clear']['text'], 
+               func=self.clear,
+               col=OPERATORS['clear']['col'], 
+               row=OPERATORS['clear']['row'],
+               font=main_font)
+
+        Button(parent=self, 
+               text=OPERATORS['percent']['text'], 
+               func=self.percent,
+               col=OPERATORS['percent']['col'], 
+               row=OPERATORS['percent']['row'],
+               font=main_font)
+
+    def clear(self):
+        print('clearx')
+
+    def percent(self):
+        print('percent')
+
+class OutputLabel(ctk.CTkLabel):
+    def __init__(self, parent, row, anchor, font,string_var):
+        super().__init__(master=parent, font= font, textvariable = string_var)
+        self.grid(column = 0, columnspan = 4, row = row, sticky = anchor,padx = 10)
 
 # Main program
-Calculator(darkdetect.isDark())
+if __name__ == '__main__':
+    Calculator(darkdetect.isDark())
