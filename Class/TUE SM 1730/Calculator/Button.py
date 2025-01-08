549b29ed11ebae038d2d55a34f036646c9bc457d
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-01-08 15:59:49 +0800

    2025-01-07 GUI - (Part 5) [pg 8]

--- Class/TUE SM 1730/Calculator/Button.py
+++ Class/TUE SM 1730/Calculator/Button.py
@@ -13,4 +13,20 @@ class Button(CTkButton):
             hover_color = COLORS[color]['hover'],
             text_color = COLORS[color]['text']
         )
-        self.grid(column = col, row = row, sticky = 'NSEW')
+        self.grid(column = col, row = row, sticky = 'NSEW',
+                  padx = STYLING['gap'], pady = STYLING['gap'])
+
+class ImageButton(CTkButton) :
+    def __init__(self, parent, func, col, row, image, text = '', color = 'dark-gray'):
+        super().__init__(
+            master = parent,
+            command = func,
+            text = text,
+            corner_radius = STYLING['corner-radius'],
+            image = image,
+            fg_color = COLORS[color]['fg'],
+            hover_color = COLORS[color]['hover'],
+            text_color = COLORS[color]['text']
+        )
+        self.grid(column = col, row = row, sticky = 'NSEW',
+                  padx = STYLING['gap'], pady = STYLING['gap'])
