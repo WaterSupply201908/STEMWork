15d2a00a9e78d19f1e4239d2b0410b1af764e28b
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-01-12 00:26:19 +0800

    2025-01-11 GUI - (Part 2) [pg 28]

--- Class/SAT TW 1530/BMI/bmi.py
+++ Class/SAT TW 1530/BMI/bmi.py
@@ -3,8 +3,10 @@
 
 import customtkinter as ctk
 
-class App(ctk.CTk) :
-  def __init__(self) :
+
+class App(ctk.CTk):
+
+  def __init__(self):
     super().__init__(fg_color='green')
 
     self.title('BMI')
@@ -12,22 +14,28 @@ class App(ctk.CTk) :
     self.resizable(False, False)
 
     self.columnconfigure(0, weight=1)
-    self.rowconfigure((0, 1, 2, 3), weight=1, uniform='a')  
-    
+    self.rowconfigure((0, 1, 2, 3), weight=1, uniform='a')
+
     ResultText(self)
     WeightInput(self)
-  
+    HeightInput(self)
+    UnitSwitcher(self)
+
     self.mainloop()
 
-class ResultText(ctk.CTkLabel) :
-  def __init__(self, parent) :
+
+class ResultText(ctk.CTkLabel):
+
+  def __init__(self, parent):
     font = ctk.CTkFont(family='Calibri', size=150, weight='bold')
     super().__init__(master=parent, text='22.5', font=font, text_color='white')
 
     self.grid(column=0, row=0, rowspan=2, sticky='nsew')
 
-class WeightInput(ctk.CTkFrame) :
-  def __init__(self, parent) :
+
+class WeightInput(ctk.CTkFrame):
+
+  def __init__(self, parent):
     super().__init__(master=parent, fg_color='White')
 
     self.grid(column=0, row=2, sticky='nsew', padx=10, pady=10)
@@ -41,9 +49,63 @@ class WeightInput(ctk.CTkFrame) :
 
     font = ctk.CTkFont(family='Calibri', size=26)
     label = ctk.CTkLabel(self, text='70kg', text_color='black', font=font)
-
     label.grid(row=0, column=2)
-    '''pg 18'''
-  
-if __name__ == '__main__' :
+
+    minus_button = ctk.CTkButton(self,
+                                 text='-',
+                                 font=font,
+                                 text_color='black',
+                                 fg_color='light gray',
+                                 hover_color='gray')
+    minus_button.grid(row=0, column=0, sticky='ns', padx=8, pady=8)
+
+    plus_button = ctk.CTkButton(self,
+                                text='+',
+                                font=font,
+                                text_color='black',
+                                fg_color='light gray',
+                                hover_color='gray')
+    plus_button.grid(row=0, column=4, sticky='ns', padx=8, pady=8)
+
+    small_plus_button = ctk.CTkButton(self,
+                                      text='+',
+                                      font=font,
+                                      text_color='black',
+                                      fg_color='light gray',
+                                      hover_color='gray')
+    small_plus_button.grid(row=0, column=3, padx=4, pady=4)
+
+    small_minus_button = ctk.CTkButton(self,
+                                       text='-',
+                                       font=font,
+                                       text_color='black',
+                                       fg_color='light gray',
+                                       hover_color='gray')
+    small_minus_button.grid(row=0, column=1, padx=4, pady=4)
+
+
+class HeightInput(ctk.CTkFrame):
+
+  def __init__(self, parent):
+    super().__init__(master=parent, fg_color='White')
+
+    self.grid(column=0, row=3, sticky='nsew', padx=10, pady=10)
+
+    slider = ctk.CTkSlider(master=self,
+                           button_color='green',
+                           button_hover_color='gray',
+                           progress_color='green',
+                           fg_color='light gray')
+    slider.pack(side='left', fill='x', expand=True, padx=10, pady=10)
+
+    output_text = ctk.CTkLabel(self, text='1.70', text_color='black',
+                               font=ctk.CTkFont(family='Calibri', size=26))
+    output_text.pack(side='left', padx=20)
+
+class UnitSwitcher(ctk.CTkLabel) :
+  def __init__(self, parent) :
+    super().__init__(master=parent, text='metric')
+    self.place(relx=0.98, rely=0.01, anchor='ne')
+
+if __name__ == '__main__':
   App()
