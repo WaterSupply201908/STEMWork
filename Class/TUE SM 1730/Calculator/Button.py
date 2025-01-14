c0375b9ec58373bc318e58e4e700d521730427e3
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-01-15 00:02:23 +0800

    2025-01-14 GUI - (Part 5) [pg 15]

--- Class/TUE SM 1730/Calculator/Button.py
+++ Class/TUE SM 1730/Calculator/Button.py
@@ -2,7 +2,7 @@ from customtkinter import CTkButton
 from Setting import *
 
 class Button(CTkButton):
-    def __init__(self, parent, text, func, col, row, font, color = 'dark-gray'):
+    def __init__(self, parent, text, func, col, row, font, span=1, color = 'dark-gray'):
         super().__init__(
             master = parent,
             command = func,
@@ -13,7 +13,7 @@ class Button(CTkButton):
             hover_color = COLORS[color]['hover'],
             text_color = COLORS[color]['text']
         )
-        self.grid(column = col, row = row, sticky = 'NSEW',
+        self.grid(column = col, columnspan=span, row = row, sticky = 'NSEW',
                   padx = STYLING['gap'], pady = STYLING['gap'])
 
 class ImageButton(CTkButton) :
@@ -30,3 +30,16 @@ class ImageButton(CTkButton) :
         )
         self.grid(column = col, row = row, sticky = 'NSEW',
                   padx = STYLING['gap'], pady = STYLING['gap'])
+        
+class NumButton(Button) :
+    def __init__(self, parent, text, func, col, row, font, span, color='light-gray') :
+        super().__init__(
+            parent=parent,
+            func=func,
+            text=text,
+            col=col,
+            row=row,
+            font=font,
+            color=color,
+            span=span
+        )
