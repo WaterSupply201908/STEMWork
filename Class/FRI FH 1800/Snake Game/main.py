837f325b93889de786e3b8022fd17bf06041f306
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-02-15 13:59:36 +0800

    2025-02-14 Snake Game - part 1 [pg 22]

---
+++ Class/FRI FH 1800/Snake Game/main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((400, 500))
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill((0, 0, 255))
+test_rect = test_surface.get_rect(center=(200, 250))
+
+while True :
+    for e in pygame.event.get() :
+        if e.type == pygame.QUIT :
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(pygame.Color('gold'))
+    test_rect.right += 1
+    screen.blit(test_surface, test_rect)
+
+    pygame.display.update()
+    clock.tick(60)
