eda806bb30b06d877b80a84080c8d2177ee88f30
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-02-21 19:35:14 +0800

    2025-02-21 Snake Game - part 2 [pg 48]

--- Class/FRI FH 1800/Snake Game/main.py
+++ Class/FRI FH 1800/Snake Game/main.py
@@ -1,13 +1,44 @@
 import pygame
 import sys
+import random
+from pygame import Vector2
+
+class Snake :
+    def __init__(self) :
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1, 0)
+
+    def draw_snake(self) :
+        for block in self.body :
+            block_rect = pygame.Rect(block.x*cell_size, block.y*cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 191, 122), block_rect)
+
+    def move_snake(self) :
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0]+self.direction)
+        self.body = body_copy
+
+class Fruit :
+    def __init__(self) :
+        self.x = random.randint(0, cell_number-1)
+        self.y = random.randint(0, cell_number-1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self) :
+        fruit_rect = pygame.Rect(self.pos.x*cell_size, self.pos.y*cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill((0, 0, 255))
-test_rect = test_surface.get_rect(center=(200, 250))
+fruit = Fruit()
+snake = Snake()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 while True :
     for e in pygame.event.get() :
@@ -15,9 +46,10 @@ while True :
             pygame.quit()
             sys.exit()
 
-    screen.fill(pygame.Color('gold'))
-    test_rect.right += 1
-    screen.blit(test_surface, test_rect)
+    screen.fill((175, 215, 70))
+
+    fruit.draw_fruit()
+    snake.draw_snake()
 
     pygame.display.update()
     clock.tick(60)
