533164a3fe1d31ba4b43321abaf69b30df230a57
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-02-22 11:06:42 +0800

    2025-02-22 Introduction to Pygame - part 3 [pg 68]

--- Class/SAT PE 0930/Introduction to Pygame/main.py
+++ Class/SAT PE 0930/Introduction to Pygame/main.py
@@ -9,28 +9,41 @@ test_font = pygame.font.Font('Runner/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('Runner/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('Runner/ground.png').convert_alpha()
-text_surface = test_font.render('My Game', False, 'Black')
+score_surface = test_font.render('My Game', False, 'Black')
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('Runner/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300))
 
 player_surface = pygame.image.load('Runner/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(80, 300))
+player_gravity = 0
 
 while True :
   for e in pygame.event.get() :
     if e.type == pygame.QUIT :
       pygame.quit()
       exit()
+    if e.type == pygame.KEYDOWN :
+      if e.key == pygame.K_SPACE :
+        player_gravity = -20
 
   screen.blit(sky_surface, (0, 0))
   screen.blit(ground_surface, (0, 300))
-  screen.blit(text_surface, (300, 50))
-  #snail_x_pos -= 4
-  #if snail_x_pos < -100 :
-  #  snail_x_pos = 800
+  pygame.draw.rect(screen, 'pink', score_rect, border_radius=10)
+  screen.blit(score_surface, score_rect)
+  snail_rect.x -= 4
+  if snail_rect.right <= 0 :
+    snail_rect.left = 800
   screen.blit(snail_surface, snail_rect)
+  player_gravity += 1
+  player_rect.y += player_gravity
+  if player_rect.bottom >= 300 :
+    player_rect.bottom = 300
   screen.blit(player_surface, player_rect)
 
+  if player_rect.colliderect(snail_rect) :
+    print('Yes')
+  
   pygame.display.update()
   clock.tick(60)
