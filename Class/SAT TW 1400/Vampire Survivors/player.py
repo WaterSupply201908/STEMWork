8d29669a61730e4b618c36c89a5c664707dd573a
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-02-24 00:53:52 +0800

    2025-02-22 Vampire survivor - part 2 [pg 7]

--- Class/SAT TW 1400/Vampire Survivors/player.py
+++ Class/SAT TW 1400/Vampire Survivors/player.py
@@ -1,5 +1,9 @@
 from settings import *
 
-class Player(pygame.sprite.Sprite) :
+class Player(pygame.sprite.Sprite) : # parent class
+  # field : variable defined in a class
+  # method : function defined in a class
   def __init__(self, pos, groups) :
     super().__init__(groups)
+    self.image = pygame.image.load(join('Image', 'Player', 'Down', '0.png')).convert_alpha()
+    self.rect = self.image.get_frect(center = pos)
