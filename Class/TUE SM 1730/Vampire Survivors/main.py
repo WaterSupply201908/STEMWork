e5c72a0373650b8a7d182f7a507e92501868c7f6
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-02-25 19:06:42 +0800

    2025-02-25 Vampire survivor - part 2 [pg 13]

--- Class/TUE SM 1730/Vampire Survivors/main.py
+++ Class/TUE SM 1730/Vampire Survivors/main.py
@@ -1,4 +1,5 @@
 from settings import *
+from player import Player
 
 class Game :
     def __init__(self) :
@@ -10,8 +11,26 @@ class Game :
         self.clock = pygame.time.Clock()
         self.running = True
 
+        self.all_sprites = pygame.sprite.Group()
+        self.player = Player((400, 300), self.all_sprites)
+
     def run(self) :
         while self.running :
-            pass
+            dt = self.clock.tick() / 1000
+
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT :
+                    self.running = False
+
+            # Game logic
+            self.all_sprites.update(dt)
+
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
 
         pygame.quit()
+
+# Main Program
+game = Game()
+game.run()
