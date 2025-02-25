3a09fd1bd58019456525050698233764bb394986
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-02-25 19:07:28 +0800

    2025-02-25 Vampire survivor - part 2 [pg 13]

---
+++ Class/TUE SM 1730/Vampire Survivors/player.py
@@ -0,0 +1,17 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite) :
+    def __init__(self, pos, groups) :
+        super().__init__(groups)
+        self.image = pygame.image.load(join("Image", "Player", "Down", "0.png")).convert_alpha()
+        self.rect = self.image.get_frect(center=pos)
+
+        self.direction = pygame.Vector2(1, 0)
+        self.speed = 500
+
+    def input(self) :
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+
+    def move(self, dt) :
+        self.rect.center += self.direction * self.speed * dt
