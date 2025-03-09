af77dc0e7e051a237fe6cc817f44491c6e8cf067
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-03-09 23:12:06 +0800

    2025-03-08 Vampire survivor - part 4 [pg 17]

--- Class/SAT TW 1400/Vampire Survivors/player.py
+++ Class/SAT TW 1400/Vampire Survivors/player.py
@@ -7,3 +7,15 @@ class Player(pygame.sprite.Sprite) : # parent class
     super().__init__(groups)
     self.image = pygame.image.load(join('VampireSurvivors', 'Image', 'Player', 'Down', '0.png')).convert_alpha()
     self.rect = self.image.get_frect(center = pos)
+
+    self.direction = pygame.Vector2(1, 0)
+    self.speed = 500
+
+  def input(self) :
+    keys = pygame.key.get_pressed()
+    self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    self.direction = self.direction.normalize() if self.direction else self.direction
+
+  def move(self, dt) :
+    self.rect.center += self.direction * self.speed * dt
