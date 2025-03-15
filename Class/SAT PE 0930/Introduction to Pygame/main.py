c3727bb9a8d4e9277aec7da84ac2db48a1485739
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-03-15 11:10:23 +0800

    2025-03-15 Introduction to Pygame - part 6 [pg 131]

--- Class/SAT PE 0930/Introduction to Pygame/main.py
+++ Class/SAT PE 0930/Introduction to Pygame/main.py
@@ -15,7 +15,10 @@ def obstacle_movement(obstacle_list) :
     for obstacle_rect in obstacle_list :
       obstacle_rect.x -= 4
 
-      screen.blit(snail_surface, obstacle_rect)
+      if obstacle_rect.bottom == 300 :
+        screen.blit(snail_surface, obstacle_rect)
+      else :
+        screen.blit(fly_surface, obstacle_rect)
 
     obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
 
@@ -23,6 +26,14 @@ def obstacle_movement(obstacle_list) :
   else :
     return []
 
+def collision(player, obstacles) :
+  if obstacles :
+    for obstacle_rect in obstacles :
+      if player.colliderect(obstacle_rect) :
+        return False
+
+  return True
+  
 # Main program
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -36,7 +47,7 @@ ground_surface = pygame.image.load('Runner/ground.png').convert_alpha()
 #score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('Runner/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+fly_surface = pygame.image.load('Runner/Fly1.png').convert_alpha()
 
 obstacle_rect_list = []
 
@@ -71,11 +82,13 @@ while True :
         if e.key == pygame.K_SPACE and player_rect.bottom >= 300 :
           player_gravity = -25
       if e.type == obstacle_time :
-        obstacle_rect_list.append(snail_surface.get_rect(midbottom=(randint(900, 1100), 300)))
+        if randint(0, 2) :
+          obstacle_rect_list.append(snail_surface.get_rect(midbottom=(randint(900, 1100), 300)))
+        else :
+          obstacle_rect_list.append(fly_surface.get_rect(midbottom=(randint(900, 1100), 210)))
     else :
       if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE :
         game_active = True
-        snail_rect.left = 800
         start_time = int(pygame.time.get_ticks()/1000)
 
   if game_active :
@@ -84,10 +97,7 @@ while True :
     #pygame.draw.rect(screen, 'pink', score_rect, border_radius=10)
     #screen.blit(score_surface, score_rect)
     score = display_score()
-    snail_rect.x -= 4
-    if snail_rect.right <= 0 :
-      snail_rect.left = 800
-    screen.blit(snail_surface, snail_rect)
+
     player_gravity += 1
     player_rect.y += player_gravity
     if player_rect.bottom >= 300 :
@@ -95,12 +105,14 @@ while True :
     screen.blit(player_surface, player_rect)
 
     obstacle_rect_list = obstacle_movement(obstacle_rect_list)
-
-    if player_rect.colliderect(snail_rect) :
-      game_active = False
+    
+    game_active = collision(player_rect, obstacle_rect_list)
   else :
     screen.fill((94, 129, 162))
     screen.blit(player_stand, player_stand_rect)
+    obstacle_rect_list.clear()
+    player_rect.midbottom = (80, 300)
+    player_gravity = 0
     score_message = test_font.render(f'Your score : {score}', False, (111, 196, 169))
     score_message_rect = score_message.get_rect(center=(400, 330))
     screen.blit(game_name, game_name_rect)
