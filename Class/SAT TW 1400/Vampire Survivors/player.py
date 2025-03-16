6f9bb19982e3c3398aac4f35834c888d238d761c
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-03-17 01:01:38 +0800

    2025-03-15 Vampire survivor - part 5 [pg 22]

--- Class/SAT TW 1400/Vampire Survivors/player.py
+++ Class/SAT TW 1400/Vampire Survivors/player.py
@@ -3,13 +3,14 @@ from settings import *
 class Player(pygame.sprite.Sprite) : # parent class
   # field : variable defined in a class
   # method : function defined in a class
-  def __init__(self, pos, groups) :
+  def __init__(self, pos, groups, collision_sprites) :
     super().__init__(groups)
     self.image = pygame.image.load(join('VampireSurvivors', 'Image', 'Player', 'Down', '0.png')).convert_alpha()
     self.rect = self.image.get_frect(center = pos)
 
     self.direction = pygame.Vector2(1, 0)
     self.speed = 500
+    self.collision_sprites = collision_sprites
 
   def input(self) :
     keys = pygame.key.get_pressed()
@@ -17,5 +18,16 @@ class Player(pygame.sprite.Sprite) : # parent class
     self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
     self.direction = self.direction.normalize() if self.direction else self.direction
 
+  def collision(self, direction) :
+    for sprite in self.collision_sprites :
+      if sprite.rect.colliderect(self.rect) :
+        if direction == 'horizontal' :
+          pass
+        else :
+          pass
+
   def move(self, dt) :
-    self.rect.center += self.direction * self.speed * dt
+    self.rect.x += self.direction.x * self.speed * dt
+    self.collision('horizontal')
+    self.rect.y += self.direction.y * self.speed * dt
+    self.collision('vertical')
