6354c41c2fcac0b675b575a6cf57f497894635e4
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-03-17 18:52:06 +0800

    2025-03-17 Introduction to Pygame - part 9 [pg 158]

--- Class/MON PE 1700/Introduction to Pygame/main.py
+++ Class/MON PE 1700/Introduction to Pygame/main.py
@@ -32,7 +32,34 @@ class Player(pygame.sprite.Sprite) : # parent class (pygame.sprite.Sprite) -> ch
         self.animation_state()
 
     def animation_state(self) :
-        pass
+        if self.rect.bottom < 300 :
+            self.image = self.player_jump
+        else :
+            self.player_index += 0.1
+
+            if self.player_index >= len(self.play_walk) :
+                self.player_index = 0
+            
+            self.image = self.play_walk[int(self.player_index)]
+
+class Obstacle(pygame.sprite.Sprite) :
+    def __init__(self, type) :
+        super().__init__()
+
+        if type == 'fly' :
+            fly_1 = pygame.image.load('fly1.png').convert_alpha()
+            fly_2 = pygame.image.load('fly2.png').convert_alpha()
+            self.frames = [fly_1, fly_2]
+            y_pos = 210
+        else : # snail
+            snail_1 = pygame.image.load('snail1.png').convert_alpha()
+            snail_2 = pygame.image.load('snail2.png').convert_alpha()
+            self.frames = [snail_1, snail_2]
+            y_pos = 300
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
 
 def display_score() :
     current_time = int(pygame.time.get_ticks()/1000) - start_time
