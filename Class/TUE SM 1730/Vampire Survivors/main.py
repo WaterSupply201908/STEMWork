c6ff03983b42ae1b53632b1ba9f2e010728ba06b
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-04-01 19:14:20 +0800

    2025-04-01 Vampire survivor - part 4 [pg 30]

--- Class/TUE SM 1730/Vampire Survivors/main.py
+++ Class/TUE SM 1730/Vampire Survivors/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from random import randint
+from pytmx.util_pygame import load_pygame
 
 class Game :
     def __init__(self) :
@@ -16,12 +17,21 @@ class Game :
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
+        self.setup()
+    
         self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
 
         for i in range(6) :
             x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
             w, h = randint(60, 100), randint(50, 120)
             CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
+
+    def setup(self) :
+        map = load_pygame(join('Data', 'Maps', 'world.tmx'))
+        for obj in map.get_layer_by_name('Objects') :
+            print(obj.x)
+            print(obj.y)
+            print(obj.image)
 
     def run(self) :
         while self.running :
