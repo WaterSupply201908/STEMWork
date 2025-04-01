7ae91d849cf3568295d46630fef4990f21a9c595
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-04-01 19:15:14 +0800

    2025-04-01 Vampire survivor - part 4 [pg 30]

--- Class/TUE SM 1730/Vampire Survivors/player.py
+++ Class/TUE SM 1730/Vampire Survivors/player.py
@@ -1,13 +1,15 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite) :
-    def __init__(self, pos, groups) :
+    def __init__(self, pos, groups, collision_sprites) :
         super().__init__(groups)
         self.image = pygame.image.load(join("Image", "Player", "Down", "0.png")).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
+        self.hitbox_rect = self.rect.inflate(-40, 0)
 
         self.direction = pygame.Vector2(1, 0)
         self.speed = 500
+        self.collision_sprites = collision_sprites
 
     def input(self) :
         keys = pygame.key.get_pressed()
@@ -16,4 +18,26 @@ class Player(pygame.sprite.Sprite) :
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, dt) :
-        self.rect.center += self.direction * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
+
+    def collision(self, direction) :
+        for sprite in self.collision_sprites :
+            if sprite.rect.colliderect(self.rect) :
+                if direction == 'horizontal' :
+                    if self.direction.x > 0 :
+                        self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0 :
+                        self.hitbox_rect.left = sprite.rect.right
+                else :
+                    if self.direction.y > 0 :
+                        self.hitbox_rect.bottom = sprite.rect.top
+                    if self.direction.y < 0 :
+                        self.hitbox_rect.top = sprite.rect.bottom
+
+    def update(self, dt) :
+        self.input()
+        self.move(dt)
