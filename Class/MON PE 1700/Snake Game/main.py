48ca0cb2fd992ff2e57ba644b19ddfb27c91f2f6
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-04-07 18:45:17 +0800

    2025-04-07 Snake Game - part 2 [pg 30]

--- Class/MON PE 1700/Snake Game/main.py
+++ Class/MON PE 1700/Snake Game/main.py
@@ -1,8 +1,19 @@
 import pygame
 import sys
 
+class Fruit :
+    def __init__(self) :
+        # create x and y pos
+        self.x = 5
+        self.y = 4
+        self.pos = pygame.math.Vector2(self.x, self.y)
+        # draw a square
+
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+CELL_SIZE = 40
+CELL_NUMBER = 20
+screen = pygame.display.set_mode((CELL_NUMBER*CELL_SIZE, CELL_NUMBER*CELL_SIZE))
+clock = pygame.time.Clock()
 
 while True :
     for e in pygame.event.get() :
@@ -11,4 +22,7 @@ while True :
             sys.exit()
 
     # Game Logic
+    screen.fill((175, 215, 70))
+
     pygame.display.update()
+    clock.tick(60)
