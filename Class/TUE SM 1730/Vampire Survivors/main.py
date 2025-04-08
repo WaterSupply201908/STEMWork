aeac8c8728b3f06a1c9950ac965ed89ddec565cc
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-04-08 19:08:40 +0800

    2025-04-08 Vampire survivor - part 5 [pg 44]

--- Class/TUE SM 1730/Vampire Survivors/main.py
+++ Class/TUE SM 1730/Vampire Survivors/main.py
@@ -19,19 +19,19 @@ class Game :
 
         self.setup()
     
-        self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
-
-        for i in range(6) :
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 120)
-            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
+        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
 
     def setup(self) :
         map = load_pygame(join('Data', 'Maps', 'world.tmx'))
+    
+        for x, y, image in map.get_layer_by_name('Ground').tiles() :
+            Sprite((x*TILE_SIZE, y*TILE_SIZE), image, self.all_sprites)
+
         for obj in map.get_layer_by_name('Objects') :
-            print(obj.x)
-            print(obj.y)
-            print(obj.image)
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+        for obj in map.get_layer_by_name('Collisions') :
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), (self.collision_sprites))
 
     def run(self) :
         while self.running :
