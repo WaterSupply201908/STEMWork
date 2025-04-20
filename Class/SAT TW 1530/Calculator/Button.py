275f078ba1b94771bbf27dbb72f3a42398181e9f
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-04-21 01:07:39 +0800

    2025-04-19 GUI - (Part 7) [pg 24]

--- Class/SAT TW 1530/Calculator/Button.py
+++ Class/SAT TW 1530/Calculator/Button.py
@@ -1,31 +1,62 @@
 from customtkinter import CTkButton
 from Setting import *
 
-class Button(CTkButton) :
-  def __init__(self, parent, text, func, col, row, font, color='dark-gray') :
-    super().__init__(
-      master=parent, 
-      command=func,
-      text=text,
-      corner_radius=STYLING['corner-radius'],
-      font=font,
-      fg_color=COLORS[color]['fg'],
-      hover_color=COLORS[color]['hover'],
-      text_color=COLORS[color]['text']
-    )
-    self.grid(column=col, row=row, sticky='NSEW')
+class Button(CTkButton):
+  def __init__(self, parent, text, func, col, row, font, span=1, color='dark-gray'):
+    super().__init__(master=parent,
+                     command=func,
+                     text=text,
+                     corner_radius=STYLING['corner-radius'],
+                     font=font,
+                     fg_color=COLORS[color]['fg'],
+                     hover_color=COLORS[color]['hover'],
+                     text_color=COLORS[color]['text'])
+    self.grid(column=col, columnspan=span, row=row, sticky='NSEW')
 
-class ImageButton(CTkButton) :
-  def __init__(self, parent, func, col, row, image, text='', color='dark-gray') :
+class ImageButton(CTkButton):
+  def __init__(self,
+               parent,
+               func,
+               col,
+               row,
+               image,
+               text='',
+               color='dark-gray'):
+    super().__init__(master=parent,
+                     command=func,
+                     text=text,
+                     corner_radius=STYLING['corner-radius'],
+                     image=image,
+                     fg_color=COLORS[color]['fg'],
+                     hover_color=COLORS[color]['hover'],
+                     text_color=COLORS[color]['text'])
+    self.grid(column=col,
+              row=row,
+              sticky='NSEW',
+              padx=STYLING['gap'],
+              pady=STYLING['gap'])
+
+class NumButton(Button):
+  def __init__(self, parent, text, func, col, row, font, span, color='light-gray'):
     super().__init__(
-      master=parent,
-      command=func,
+      parent=parent,
+      func=lambda:func(text),
       text=text,
-      corner_radius=STYLING['corner-radius'],
-      image=image,
-      fg_color=COLORS[color]['fg'],
-      hover_color=COLORS[color]['hover'],
-      text_color=COLORS[color]['text']
+      col=col,
+      row=row,
+      font=font,
+      color=color,
+      span=span
     )
-    self.grid(column=col, row=row, sticky='NSEW',
-              padx=STYLING['gap'], pady=STYLING['gap'])
+
+class MathButton(Button) :
+  def __init__(self, parent, text, operator, func, col, row, font, color='orange') :
+    super().__init__(
+      parent=parent,
+      func=lambda:func(operator),
+      text=text,
+      col=col,
+      row=row,
+      font=font,
+      color=color
+    )
