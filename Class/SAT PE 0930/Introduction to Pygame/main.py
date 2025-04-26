6299749d9c08151fc19c30ab900db434fe8c40ed
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-04-26 10:39:18 +0800

    2025-04-26 Introduction to Pygame - part 10 [Completed]

--- Class/SAT PE 0930/Introduction to Pygame/main.py
+++ Class/SAT PE 0930/Introduction to Pygame/main.py
@@ -17,11 +17,14 @@ class Player(pygame.sprite.Sprite) :
     self.image = self.player_walk[self.player_index]
     self.rect = self.image.get_rect(midbottom=(80, 300))
     self.gravity = 0
+    self.jump_sound = pygame.mixer.Sound('Runner/jump.mp3')
+    self.jump_sound.set_volume(0.9)
 
   def player_input(self) :
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE] and self.rect.bottom >= 300 :
       self.gravity = -20
+      self.jump_sound.play()
 
   def apply_gravity(self) :
     self.gravity += 1
@@ -193,6 +196,9 @@ pygame.time.set_timer(fly_animation_timer, 200)
 game_active = False
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('Runner/music.wav')
+bg_music.set_volume(0.5)
+bg_music.play(loops=-1)
 
 while True :
   for e in pygame.event.get() :
