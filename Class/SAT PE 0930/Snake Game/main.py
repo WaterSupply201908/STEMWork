28bd05b535a01e42faf52b294484bb9130ef62cc
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-03 10:45:58 +0800

    2025-05-03 Snake Game - part 2 [pg 46]

--- Class/SAT PE 0930/Snake Game/main.py
+++ Class/SAT PE 0930/Snake Game/main.py
@@ -1,14 +1,41 @@
 import pygame
+from pygame import Vector2
 import sys
+import random
+
+CELL_SIZE, CELL_NUMBER = 40, 20
+
+class Snake :
+  def __init__(self) :
+    self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+    self.direction = Vector2(1, 0)
+
+  def draw_snake(self) :
+    for block in self.body :
+      block_rect = pygame.Rect(block.x*CELL_SIZE, block.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
+      pygame.draw.rect(screen, (183, 191, 122), block_rect)
+
+class Fruit :
+  def __init__(self) :
+    # create x & y pos
+    self.x = random.randint(0, CELL_NUMBER-1)
+    self.y = random.randint(0, CELL_NUMBER-1)
+    self.pos = Vector2(self.x, self.y)
+  # draw a square
+  def draw_fruit(self) :
+    fruit_rect = pygame.Rect(self.pos.x*CELL_SIZE, self.pos.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 500))
+screen = pygame.display.set_mode((CELL_SIZE*CELL_NUMBER, CELL_SIZE*CELL_NUMBER))
 clock = pygame.time.Clock()
+fruit = Fruit()
+snake = Snake()
 
 test_surface = pygame.Surface((100, 200))
 test_surface.fill((0, 0, 255))
-test_rect = pygame.Rect(100, 200, 100, 100)
+test_rect = test_surface.get_rect(center=(200, 250))
 
 while True :
   for e in pygame.event.get() :
@@ -16,9 +43,9 @@ while True :
       pygame.quit()
       sys.exit()
 
-  screen.fill(pygame.Color('gold'))
-  pygame.draw.rect(screen, (255, 0, 0), test_rect)
-  screen.blit(test_surface, (200, 200))
+  screen.fill((175, 215, 70))
+  fruit.draw_fruit()
+  snake.draw_snake()
   
   pygame.display.update()
   clock.tick(60)
