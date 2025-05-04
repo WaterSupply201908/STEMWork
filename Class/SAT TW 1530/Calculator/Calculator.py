3a0a2c4883d7e9013a424a0dff8e5fd9e40f0eb0
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-05 00:48:08 +0800

    2025-05-03 GUI - (Part 9) [Completed]

--- Class/SAT TW 1530/Calculator/Calculator.py
+++ Class/SAT TW 1530/Calculator/Calculator.py
@@ -4,8 +4,8 @@ from Setting import *
 from Button import Button, ImageButton, NumButton, MathButton, MathImageButton
 from PIL import Image
 
-class Calculator(ctk.CTk) :
-  def __init__(self, is_dark) :
+class Calculator(ctk.CTk):
+  def __init__(self, is_dark):
     super().__init__(fg_color=(WHITE, BLACK))
 
     ctk.set_appearance_mode(f'{"dark" if is_dark else "light"}')
@@ -19,12 +19,13 @@ class Calculator(ctk.CTk) :
     self.result_string = ctk.StringVar(value='0')
     self.formula_string = ctk.StringVar(value='')
     self.display_nums = []
+    self.full_operation = []
 
     self.create_widgets()
 
     self.mainloop()
 
-  def create_widgets(self) :
+  def create_widgets(self):
     main_font = ctk.CTkFont(family=FONT, size=NORMAL_FONT_SIZE)
     result_font = ctk.CTkFont(family=FONT, size=OUTPUT_FONT_SIZE)
 
@@ -32,11 +33,11 @@ class Calculator(ctk.CTk) :
     OutputLabel(self, 1, 'E', result_font, self.result_string)
 
     Button(parent=self,
-             text=OPERATORS['clear']['text'],
-             func=self.clear,
-             col=OPERATORS['clear']['col'],
-             row=OPERATORS['clear']['row'],
-             font=main_font)
+           text=OPERATORS['clear']['text'],
+           func=self.clear,
+           col=OPERATORS['clear']['col'],
+           row=OPERATORS['clear']['row'],
+           font=main_font)
 
     Button(parent=self,
            text=OPERATORS['percent']['text'],
@@ -46,76 +47,102 @@ class Calculator(ctk.CTk) :
            font=main_font)
 
     invert_image = ctk.CTkImage(
-      dark_image=Image.open(OPERATORS['invert']['image path']['light']),
-      light_image=Image.open(OPERATORS['invert']['image path']['dark'])
-    )
-    ImageButton(
-      parent=self,
-      text='',
-      func=self.invert,
-      col=OPERATORS['invert']['col'],
-      row=OPERATORS['invert']['row'],
-      image=invert_image
-    )
+        dark_image=Image.open(OPERATORS['invert']['image path']['light']),
+        light_image=Image.open(OPERATORS['invert']['image path']['dark']))
+    ImageButton(parent=self,
+                text='',
+                func=self.invert,
+                col=OPERATORS['invert']['col'],
+                row=OPERATORS['invert']['row'],
+                image=invert_image)
 
-    for num, data in NUM_POSITIONS.items() :
-      NumButton(
-        parent=self,
-        text=num,
-        func=self.num_press,
-        col=data['col'],
-        row=data['row'],
-        font=main_font,
-        span=data['span']
-      )
+    for num, data in NUM_POSITIONS.items():
+      NumButton(parent=self,
+                text=num,
+                func=self.num_press,
+                col=data['col'],
+                row=data['row'],
+                font=main_font,
+                span=data['span'])
 
-    for operator, data in MATH_POSITIONS.items() :
-      if data['image path'] :
-        MathImageButton(
-          parent=self,
-          operator=operator,
-          func=self.math_press,
-          col=data['col'],
-          row=data['row'],
-          image=ctk.CTkImage(
-            light_image=Image.open(data['image path']['dark']),
-            dark_image=Image.open(data['image path']['light'])
-          )
-        )
-      else :
-        MathButton(
-          parent=self,
-          text=data['character'],
-          operator=operator,
-          func=self.math_press,
-          col=data['col'],
-          row=data['row'],
-          font=main_font
-        )
+    for operator, data in MATH_POSITIONS.items():
+      if data['image path']:
+        MathImageButton(parent=self,
+                        operator=operator,
+                        func=self.math_press,
+                        col=data['col'],
+                        row=data['row'],
+                        image=ctk.CTkImage(light_image=Image.open(
+                            data['image path']['dark']),
+                                           dark_image=Image.open(
+                                               data['image path']['light'])))
+      else:
+        MathButton(parent=self,
+                   text=data['character'],
+                   operator=operator,
+                   func=self.math_press,
+                   col=data['col'],
+                   row=data['row'],
+                   font=main_font)
 
-  def clear(self) :
-    print('clear')
+  def clear(self):
+    self.result_string.set('0')
+    self.formula_string.set('')
+    self.display_nums.clear()
+    self.full_operation.clear()
 
-  def percent(self) :
-    print('percent')
+  def percent(self):
+    if self.display_nums :
+      current_number = float(''.join(self.display_nums))
+      percent_value = current_number / 100
+
+      self.display_nums = list(str(percent_value))
+      self.result_string.set(''.join(self.display_nums))
 
   def invert(self) :
-    print('invert')
+    current_number = ''.join(self.display_nums)
+    if current_number :
+      if float(current_number) > 0 :
+        self.display_nums.insert(0, '-')
+      else :
+        self.display_nums.pop(0)
+
+    self.result_string.set(''.join(self.display_nums))
 
   def num_press(self, value) :
     self.display_nums.append(f'{value}')
     full_number = ''.join(self.display_nums)
     self.result_string.set(full_number)
 
-  def math_press(self, value) :
+  def math_press(self, value):
     current_number = ''.join(self.display_nums)
-    if current_number :
-      print('yes')
+    if current_number:
+      self.full_operation.append(current_number)
+      if value != '=':
+        self.full_operation.append(value)
+        self.display_nums.clear()
+        self.result_string.set('')
+        self.formula_string.set(''.join(self.full_operation))
+      else:
+        formula = ''.join(self.full_operation)
+        result = eval(formula)
 
-class OutputLabel(ctk.CTkLabel) :
-  def __init__(self, parent, row, anchor, font, string_var) :
+        if isinstance(result, float):
+          if result.is_integer():
+            result = int(result)
+          else:
+            result = round(result, 3)
+
+        self.full_operation.clear()
+        self.display_nums = [str(result)]
+
+        self.formula_string.set(formula)
+        self.result_string.set(result)
+
+class OutputLabel(ctk.CTkLabel):
+  def __init__(self, parent, row, anchor, font, string_var):
     super().__init__(master=parent, font=font, textvariable=string_var)
     self.grid(column=0, columnspan=4, row=row, sticky=anchor, padx=10)
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
   Calculator(darkdetect.isDark())
