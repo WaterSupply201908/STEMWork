1c8314040802152f24c4a6be71f620f293820b44
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-09 19:47:02 +0800

    2025-05-09 Vampire survivor - part 3 [pg 46]

--- Class/FRI FH 1800/Vampire Survivors/main.py
+++ Class/FRI FH 1800/Vampire Survivors/main.py
@@ -3,6 +3,7 @@ from player import Player
 from sprite import *
 from random import randint
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 class Game :
     def __init__(self):
@@ -12,20 +13,23 @@ class Game :
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
 
-        self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
-        for i in range(6) :
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 120)
-            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
+        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
 
     def setup(self) :
         map = load_pygame(join('data', 'maps', 'world.tmx'))
-        print(map)
+        for x, y, image in map.get_layer_by_name('Ground').tiles() :
+            Sprite((x*TILE_SIZE, y*TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects') :
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+        for obj in map.get_layer_by_name('Collisions') :
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), (self.collision_sprites))
 
     def run(self) :
         while self.running :
@@ -38,7 +42,7 @@ class Game :
             self.all_sprites.update(dt)
 
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw()
             pygame.display.update()
 
         pygame.quit()
