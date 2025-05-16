565a9acd8214d5e562941d9fda4c7b187d31bf6e
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-16 19:38:24 +0800

    2025-05-16 Vampire survivor - part 4 [pg 58]

--- Class/FRI FH 1800/Vampire Survivors/main.py
+++ Class/FRI FH 1800/Vampire Survivors/main.py
@@ -18,8 +18,6 @@ class Game :
 
         self.setup()
 
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
-
     def setup(self) :
         map = load_pygame(join('data', 'maps', 'world.tmx'))
         for x, y, image in map.get_layer_by_name('Ground').tiles() :
@@ -30,6 +28,10 @@ class Game :
 
         for obj in map.get_layer_by_name('Collisions') :
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), (self.collision_sprites))
+
+        for obj in map.get_layer_by_name('Entities') :
+            if obj.name == 'Player' :
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self) :
         while self.running :
@@ -42,7 +44,7 @@ class Game :
             self.all_sprites.update(dt)
 
             self.display_surface.fill('black')
-            self.all_sprites.draw()
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
