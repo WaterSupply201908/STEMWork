8645febc145f53dd64e0485fb9ec4129b2dd5050
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-20 19:14:14 +0800

    2025-05-20 Vampire survivor - part 6 [pg 55]

--- Class/TUE SM 1730/Vampire Survivors/main.py
+++ Class/TUE SM 1730/Vampire Survivors/main.py
@@ -3,6 +3,7 @@ from player import Player
 from sprites import *
 from random import randint
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 class Game :
     def __init__(self) :
@@ -14,12 +15,10 @@ class Game :
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
-    
-        self.player = Player((500, 300), self.all_sprites, self.collision_sprites)
 
     def setup(self) :
         map = load_pygame(join('Data', 'Maps', 'world.tmx'))
@@ -33,6 +32,10 @@ class Game :
         for obj in map.get_layer_by_name('Collisions') :
             CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), (self.collision_sprites))
 
+        for obj in map.get_layer_by_name('Entities') :
+            if obj.name == 'Player' :
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+
     def run(self) :
         while self.running :
             dt = self.clock.tick() / 1000
@@ -45,7 +48,7 @@ class Game :
             self.all_sprites.update(dt)
 
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
