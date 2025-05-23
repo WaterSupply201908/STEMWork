35b054c11c3fab06912a689b0b07c7ff32f18062
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-23 19:43:56 +0800

    2025-05-23 Vampire survivor - part 5 [pg 70]

--- Class/FRI FH 1800/Vampire Survivors/player.py
+++ Class/FRI FH 1800/Vampire Survivors/player.py
@@ -3,6 +3,8 @@ from settings import *
 class Player(pygame.sprite.Sprite) :
     def __init__(self, pos, groups, collision_sprites) :
         super().__init__(groups)
+        self.load_images()
+        self.state, self.frame_index = 'down', 0
         self.image = pygame.image.load(join('Image', 'Player', 'Down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
         self.hitbox_rect = self.rect.inflate(-60, -60)
@@ -16,8 +18,11 @@ class Player(pygame.sprite.Sprite) :
 
         for state in self.frames.keys() :
             for folder_path, sub_folders, filenames in walk(join('Image', 'Player', state)) :
-                if file_names :
-                    pass
+                if filenames :
+                    for filename in sorted(filenames, key=lambda name : int(name.split('.')[0])) :
+                        full_path = join(folder_path, filename)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
 
     def input(self) :
         keys = pygame.key.get_pressed()
@@ -42,6 +47,28 @@ class Player(pygame.sprite.Sprite) :
                     if self.direction.y > 0 : self.rect.bottom = sprite.rect.top
                     if self.direction.y < 0 : self.rect.top = sprite.rect.bottom
 
+    def animate(self, dt) :
+        # get state
+        if self.direction.x != 0 :
+            if self.direction.x > 0 :
+                self.state = 'right'
+            else :
+                self.state = 'left'
+        if self.direction.y != 0 :
+            if self.direction.y > 0 :
+                self.state = 'down'
+            else :
+                self.state = 'up'
+
+        # animate
+        if self.direction :
+            self.frame_index += 5*dt
+        else :
+            self.frame_index = 0
+
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
     def update(self, dt) :
         self.input()
         self.move(dt)
+        self.animate(dt)
