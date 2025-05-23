36f2f1b4152d45e6eed0a5d858d121f72659876c
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-23 19:43:26 +0800

    2025-05-23 Vampire survivor - part 5 [pg 70]

--- Class/FRI FH 1800/Vampire Survivors/sprite.py
+++ Class/FRI FH 1800/Vampire Survivors/sprite.py
@@ -12,3 +12,24 @@ class CollisionSprite(pygame.sprite.Sprite) :
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft=pos)
+
+class Gun(pygame.sprite.Sprite) :
+    def __init__(self, player, groups) :
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1, 0)
+
+        super().__init__(groups)
+
+        self.gun_surf = pygame.image.load(join('Image', 'Gun', 'gun.png'))
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center=self.player.rect.center+self.player_direction*self.distance)
+
+    def get_direction(self) :
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
+
+    def update(self, _) :
+        self.get_direction()
+        self.rect.center = self.player.rect.center + self.player_direction  * self.distance
