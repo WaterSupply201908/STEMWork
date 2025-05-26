673e5119ed8bb9704489daf01ad42f93dc97d088
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-26 16:17:33 +0800

    2025-05-24 Vampire survivor - part 14 [pg 105]

--- Class/SAT TW 1400/Vampire Survivors/main.py
+++ Class/SAT TW 1400/Vampire Survivors/main.py
@@ -81,7 +81,17 @@ class Game :
         self.gun = Gun(self.player, self.all_sprites)
       else :
         self.spawn_positions.append((obj.x, obj.y))
-  
+
+  def bullet_collision(self) :
+    if self.bullet_sprites :
+      for bullet in self.bullet_sprites :
+        collision_sprites = pygame.sprite.spritecollide(bullet, self.enemy_sprites, False, pygame.sprite.collide_mask)
+        if collision_sprites :
+          for sprite in collision_sprites :
+            sprite.destroy()
+
+          bullet.kill()
+
   def run(self) :
     while self.running :
       dt = self.clock.tick() / 1000
@@ -95,6 +105,7 @@ class Game :
       self.gun_timer()
       self.input()
       self.all_sprites.update(dt)
+      self.bullet_collision()
       self.display_surface.fill('black')
       self.all_sprites.draw(self.player.rect.center)
       pygame.display.update()
