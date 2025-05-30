9149ba50e5fd8f79714bf5627ff0cf9a6eb5aa00
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-05-30 19:39:56 +0800

    2025-05-30 Vampire survivor - part 6 [pg 84]

--- Class/FRI FH 1800/Vampire Survivors/main.py
+++ Class/FRI FH 1800/Vampire Survivors/main.py
@@ -12,11 +12,17 @@ class Game :
         pygame.display.set_caption('Vampire Survivors')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_image()
 
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
 
         self.setup()
+
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
 
     def setup(self) :
         map = load_pygame(join('data', 'maps', 'world.tmx'))
@@ -34,6 +40,22 @@ class Game :
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
                 self.gun = Gun(self.player, self.all_sprites)
 
+    def load_image(self) :
+        self.bullet_surf = pygame.image.load(join('image', 'gun', 'bullet.png')).convert_alpha()
+
+    def input(self) :
+        if pygame.mouse.get_pressed()[0] and self.can_shoot :
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+
+    def gun_timer(self) :
+        if not self.can_shoot :
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown :
+                self.can_shoot = True
+
     def run(self) :
         while self.running :
             dt = self.clock.tick() / 1000
@@ -42,6 +64,8 @@ class Game :
                 if event.type == pygame.QUIT :
                     self.running = False
 
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             self.display_surface.fill('black')
