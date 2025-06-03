804c1669d1191c87feac739e7ef98b8928a4f2c1
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-06-03 19:06:50 +0800

    2025-06-03 Vampire survivor - part 8 [pg 83]

--- Class/TUE SM 1730/Vampire Survivors/sprites.py
+++ Class/TUE SM 1730/Vampire Survivors/sprites.py
@@ -1,4 +1,5 @@
 from settings import *
+from math import atan2, degrees
 
 class Sprite(pygame.sprite.Sprite) :
     def __init__(self, pos, surf, groups) :
@@ -31,6 +32,26 @@ class Gun(pygame.sprite.Sprite) :
         player_pos = pygame.Vector2(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
         self.player_direction = (mouse_pos - player_pos).normalize()
 
+    def rotate_gun(self) :
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0 :
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else :
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
     def update(self, _) :
         self.get_direction()
+        self.rotate_gun()
         self.rect.center = self.player.rect.center + self.player_direction * self.distance
+
+class Bullet(pygame.sprite.Sprite) :
+    def __init__(self, surf, pos, direction, groups) :
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center=pos)
+        self.direction = direction
+        self.speed = 1200
+
+    def update(self, dt) :
+        self.rect.center += self.direction * self.speed * dt
