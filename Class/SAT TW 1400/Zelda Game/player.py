73b0734c1e6068abac49d21eff5b42c4b5e0c890
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-08-16 17:51:35 +0800

    2025-08-16 Zelda Game - part 5 [pg 116]

--- Class/SAT TW 1400/Zelda Game/player.py
+++ Class/SAT TW 1400/Zelda Game/player.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from support import import_folder
 
 class Player(pygame.sprite.Sprite) :
     def __init__(self, pos, groups, obstacle_sprites) :
@@ -8,28 +9,92 @@ class Player(pygame.sprite.Sprite) :
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -30)
+        self.import_player_assets()
+        self.status = 'down'
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
         self.obstacle_sprites = obstacle_sprites
+
+    def import_player_assets(self) :
+        character_path = "../graphics/player/"
+        self.animations = {
+            'up'           : [],
+            'down'         : [],
+            'left'         : [],
+            'right'        : [],
+            'up_idle'      : [],
+            'down_idle'    : [],
+            'left_idle'    : [],
+            'right_idle'   : [],
+            'up_attack'    : [],
+            'down_attack'  : [],
+            'left_attack'  : [],
+            'right_attack' : []
+        }
+
+        for animation in self.animations.keys() :
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
 
     def input(self) :
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP] :
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN] :
             self.direction.y = 1
+            self.status = 'down'
         else :
             self.direction.y = 0
 
         if keys[pygame.K_LEFT] :
             self.direction.x = -1
+            self.status = 'left'
         elif keys[pygame.K_RIGHT] :
             self.direction.x = 1
+            self.status = 'right'
         else :
             self.direction.x = 0
+
+        if keys[pygame.K_SPACE] and not self.attacking :
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print('attack')
+
+        if keys[pygame.K_LCTRL] and not self.attacking :
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print('magic')
+
+    def cooldown(self) :
+        current_time = pygame.time.get_ticks()
+
+        if self.attacking :
+            if current_time - self.attack_time >= self.attack_cooldown :
+                self.attacking = False
+
+    def get_status(self) :
+        if self.direction.x == 0 and self.direction.y == 0 :
+            if not 'idle' in self.status and not 'attack' in self.status :
+                self.status = self.status + '_idle'
+
+        if self.attacking :
+            self.direction.x = 0
+            self.direction.y = 0
+            if not 'attack' in self.status :
+                if 'idle' in self.status :
+                    self.status = self.status.replace('_idle', '_attack')
+                else :
+                    self.status = self.status + '_attack'
+        else :
+            if 'attack' in self.status :
+                self.status = self.status.replace('_attack', '')
 
     def collision(self, direction) :
         if direction == 'horizontal' :
@@ -59,4 +124,6 @@ class Player(pygame.sprite.Sprite) :
 
     def update(self) :
         self.input()
+        self.cooldown()
+        self.get_status()
         self.move(self.speed)
