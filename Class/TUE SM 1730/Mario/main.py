e1dfb830548b6498adf0269a190917ef96f9cfc9
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-08-19 19:08:00 +0800

    2025-08-19 Mario - part 1 [pg 11]

--- Class/TUE SM 1730/Mario/main.py
+++ Class/TUE SM 1730/Mario/main.py
@@ -1,18 +1,32 @@
 from settings import *
 import sys
 from level import Level
-
-WIDTH, HEIGHT = 600, 500
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game :
-    def __init__(self) :
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('Super World')
+         def __init__(self) :
+                 pygame.init()
+                 self.display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+                 pygame.display.set_caption('Super World')
+                 self.clock = pygame.time.Clock()
 
-    def run(self) :
-        while True :
-            for event in pygame.event.get() :
-                if event.type == pygame.QUIT :
-                    pygame.quit()
-                    sys.exit()
+                 self.tmx_maps = {0 : load_pygame(join('.', 'data', 'levels', 'omni.tmx'))}
+                 self.current_stage = Level(self.tmx_maps[0])
+
+         def run(self) :
+                 while True :
+                         for event in pygame.event.get() :
+                                 if event.type == pygame.QUIT :
+                                         pygame.quit()
+                                         sys.exit()
+                
+                         self.current_stage.run()
+                         
+                         pygame.display.update()
+                         self.clock.tick(60)
+
+# Main
+if __name__ == '__main__' :
+         game = Game()
+         game.run()
