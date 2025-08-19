f7a943567a6460183ca39df56df041617677213c
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-08-19 19:07:24 +0800

    2025-08-19 Mario - part 1 [pg 11]

--- Class/TUE SM 1730/Mario/settings.py
+++ Class/TUE SM 1730/Mario/settings.py
@@ -1,3 +1,17 @@
 import pygame
+from pygame.math import Vector2 as vector
 
 WIDTH, HEIGHT = 600, 500
+TILE_SIZE = 64
+ANIMATION_SPEED = 6
+
+Z_LAYERS = {
+         'bg' : 0,
+         'clouds' : 1,
+         'bg tiles' : 2,
+         'path' : 3,
+         'bg details' : 4,
+         'main' : 5,
+         'water' : 6,
+         'fg' : 7
+}
