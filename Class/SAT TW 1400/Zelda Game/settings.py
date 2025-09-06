27d5893a8838859fb9bde0081333f9013ceae603
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-09-06 18:11:58 +0800

    2025-09-06 Creating a Zelda style game - part 8 [pg 165]

--- Class/SAT TW 1400/Zelda Game/settings.py
+++ Class/SAT TW 1400/Zelda Game/settings.py
@@ -34,3 +34,22 @@ weapon_data = {
     'rapier' : {'cooldown':50,  'damage':8,  'graphic':'../graphics/weapons/rapier/full.png'},
     'sai'    : {'cooldown':80,  'damage':10, 'graphic':'../graphics/weapons/sai/full.png'}
 }
+
+# UI
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = '../graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# General Colors
+WATER_COLOR = '#71ddee' # hex : ff -> dec : 255
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#eeeeee'
+
+# UI Colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
