04f920217b8014ae3ffd70320170271972efc52b
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-10-13 01:17:14 +0800

    2025-10-11 Creating a Zelda style game - part 9 [pg 173]

--- Class/SAT TW 1400/Zelda Game/ui.py
+++ Class/SAT TW 1400/Zelda Game/ui.py
@@ -3,6 +3,12 @@ from settings import *
 
 class UI :
     def __init__(self) :
+        self.weapon_graphics = []
+        for weapon in weapon_data.values() :
+            path = weapon['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
         self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
 
@@ -26,10 +32,30 @@ class UI :
         y = self.display_surface.get_size()[1] - 20
         text_rect = text_surf.get_rect(bottomright=(x, y))
 
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, text_rect.inflate(20, 20))
         self.display_surface.blit(text_surf, text_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, text_rect.inflate(20, 20), 3)
+
+    def selection_box(self, left, top) :
+        bg_rect = pygame.Rect(left, top, ITEM_BOX_SIZE, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+        pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
+
+        return bg_rect
+
+    def weapon_overlay(self, weapon_index) :
+        bg_rect = self.selection_box(10, 630)
+
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center=bg_rect.center)
+
+        self.display_surface.blit(weapon_surf, weapon_rect)
 
     def display(self, player) :
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
 
         self.show_exp(player.exp)
+
+        self.weapon_overlay(player.weapon_index)
+        self.selection_box(80, 635)
