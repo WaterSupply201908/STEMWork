fccdf9d3c6cf372220e059d7e3ce81c4079154af
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-10-19 23:34:37 +0800

    2025-10-18 Creating a Zelda style game - part 10 [pg 189]
    Added magic data for flame and heal spells.

--- Class/SAT TW 1400/Zelda Game/settings.py
+++ Class/SAT TW 1400/Zelda Game/settings.py
@@ -35,6 +35,11 @@ weapon_data = {
     'sai'    : {'cooldown':80,  'damage':10, 'graphic':'../graphics/weapons/sai/full.png'}
 }
 
+magic_data = {
+    'flame' : {'strength':5, 'cost':20, 'graphic':'../graphics/particles/flame/fire.png'},
+    'heal'  : {'strength':20,'cost':10, 'graphic':'../graphics/particles/heal/heal.png'}
+}
+
 # UI
 BAR_HEIGHT = 20
 HEALTH_BAR_WIDTH = 200
