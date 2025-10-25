9670cc50791959ce70de3ce45fd957a6bcbc3904
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-10-25 18:09:30 +0800

    2025-10-25 Creating a Zelda style game - part 11 [pg 206]

--- Class/SAT TW 1400/Zelda Game/level.py
+++ Class/SAT TW 1400/Zelda Game/level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level :
   def __init__(self) :
@@ -40,7 +41,8 @@ class Level :
     layouts = {
       'boundary' : import_csv_layout('../map/map_FloorBlocks.csv'),
       'grass' : import_csv_layout('../map/map_Grass.csv'),
-      'object' : import_csv_layout('../map/map_LargeObjects.csv')
+      'object' : import_csv_layout('../map/map_LargeObjects.csv'),
+      'entities' : import_csv_layout('../map/map_Entities.csv')
     }
 
     graphics = {
@@ -63,6 +65,14 @@ class Level :
             elif style == 'object' :
               surf = graphics['objects'][int(col)]
               Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+            elif style == 'entities' :
+              if col == '394' :
+                self.player = Player(
+                  (x, y),
+                  [self.visible_sprites],
+                  self.obstacle_sprites, self.create_attack, self.destroy_attack, self.create_magic)
+              else :
+                Enemy('squid', (x, y), [self.visible_sprites])
 
   def create_attack(self) :
     self.current_attack = Weapon(self.player, [self.visible_sprites])
