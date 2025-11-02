5a5e0c441145e8d0bcc55ed11af12764556d087b
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-11-02 23:18:58 +0800

    2025-11-01 Creating a Zelda style game - part 12 [pg 217]
    Added obstacle_sprites parameter to Enemy class constructor and initialized monster attributes from monster_data.

--- Class/SAT TW 1400/Zelda Game/enemy.py
+++ Class/SAT TW 1400/Zelda Game/enemy.py
@@ -4,7 +4,7 @@ from entity import Entity
 from support import import_folder
 
 class Enemy(Entity) :
-    def __init__(self, monster_name, pos, groups) :
+    def __init__(self, monster_name, pos, groups, obstacle_sprites) :
         super().__init__(groups)
         self.sprite_type = 'enemy'
 
@@ -12,6 +12,19 @@ class Enemy(Entity) :
         self.status = 'idle'
         self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
+        self.obstacle_sprites = obstacle_sprites
+
+        self.monster_name = monster_name
+        monster_info = monster_data[self.monster_name]
+        self.health = monster_info['health']
+        self.exp = monster_info['exp']
+        self.speed = monster_info['speed']
+        self.attack_damage = monster_info['damage']
+        self.resistance = monster_info['resistance']
+        self.attack_radius = monster_info['attack_radius']
+        self.notice_radius = monster_info['notice_radius']
+        self.attack_type = monster_info['attack_type']
 
     def image_graphics(self, name) :
         self.animations = {'idle' : [], 'move' : [], 'attack' : []}
@@ -19,3 +32,28 @@ class Enemy(Entity) :
 
         for animation in self.animations.keys() :
             self.animations[animation] = import_folder(main_path + animation)
+
+    def get_player_distance_direction(self, player) :
+        enemy_vec = pygame.math.Vector2(self.rect.center)
+        player_vec = pygame.math.Vector2(player.rect.center)
+        distance = (player_vec - enemy_vec).magnitude()
+
+        if distance > 0 :
+            direction = (player_vec - enemy_vec).normalize()
+        else :
+            direction = pygame.math.Vector2()
+
+        return (distance, direction)
+
+    def get_status(self, player) :
+        distance = self.get_player_distance_direction(player)[0]
+
+        if distance <= self.attack_radius :
+            self.status = 'attack'
+        elif distance <= self.notice_radius :
+            self.status = 'move'
+        else :
+            self.status = 'idle'
+
+    def update(self) :
+        self.move(self.speed)
