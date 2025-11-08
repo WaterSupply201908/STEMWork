12c0fd34800468951cf29b7f7de60ea4c50480f5
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-11-08 18:26:02 +0800

    2025-11-08 Vampire survivor - part 1 [pg 10]

---
+++ Class/SAT TW 1530/Vampire Survivors/main.py
@@ -0,0 +1,30 @@
+from settings import *
+from player import Player
+
+class Game :
+    def __init__(self) :
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Vampire Survivors")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.all_sprites = pygame.sprite.Group()
+        self.player = Player((400, 300), self.all_sprites)
+
+    def run(self) :
+        while self.running :
+            dt = self.clock.tick(60) / 1000
+
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT :
+                    self.running = False
+
+            # game logic
+            self.all_sprites.update(dt)
+
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+
+game = Game()
+game.run()
