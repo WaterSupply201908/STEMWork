7ddfc8c8f5cdb512cdbf041034d11a74d7a05729
WaterSupply201908 <82951992+WaterSupply201908@users.noreply.github.com>
2025-11-16 23:16:41 +0800

    2025-11-15 Vampire survivor - part 2 [pg 22]

--- Class/SAT TW 1530/Vampire Survivors/player.py
+++ Class/SAT TW 1530/Vampire Survivors/player.py
@@ -1,8 +1,31 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite) :
-    def __init__(self, pos, groups) :
+    def __init__(self, pos, groups, collision_sprites) :
         super().__init__(groups)
 
         self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_rect(center = pos)
+
+        self.direction = pygame.math.Vector2(1, 0)
+        self.speed = 500
+        self.collision_sprites = collision_sprites
+
+    def input(self) :
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+    def collision(self, direction) :
+        pass
+
+    def move(self, dt) :
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+
+    def update(self, dt) :
+        self.input()
+        self.move(dt)
